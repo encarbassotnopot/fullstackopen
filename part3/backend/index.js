69edcbceae7653d05f7f09fa654e2f986316bd90
eina <eina@eina.cc>
2026-06-04 17:56:43 +0200

    part3.c feta

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -28,41 +28,78 @@ app.use(
 morgan.token("posts", (req, res) => JSON.stringify(req.body));
 
 app.get("/info", (req, res) => {
-	res.send(
-		`Phonebook has info for ${phoneBook.length} people<br/>${Date(Date.now())}`
-	);
+	Person.find({}).then((people) => {
+		const len = people.length;
+		res.send(
+			`Phonebook has info for ${len} people<br/>${Date(Date.now())}`
+		);
+	});
 });
 
 app.get("/api/persons", (req, res) => {
 	Person.find({}).then((people) => res.json(people));
 });
 
-app.get("/api/persons/:id", (req, res) => {
-	Person.findById(req.params.id).then((p) => res.json(p));
+app.get("/api/persons/:id", (req, res, next) => {
+	Person.findById(req.params.id)
+		.then((person) => {
+			if (person) res.json(person);
+			else res.status(404).end();
+		})
+		.catch((err) => next(err));
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-	const id = req.params.id;
-	phoneBook = phoneBook.filter((e) => e.id !== id);
-
-	res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+	Person.findByIdAndDelete(req.params.id)
+		.then((mongoRes) => res.status(204).end())
+		.catch((err) => next(err));
 });
-
-const genId = () => {
-	return String(Math.floor(Math.random() * 1_000_000_000_000));
-};
 
 app.post("/api/persons", (req, res) => {
 	const data = req.body;
 
-	if (!data.name) return res.status(400).json({ error: "missing name" });
-	if (!data.number) return res.status(400).json({ error: "missing number" });
-	//if (phoneBook.some((e) => e.name === data.name))
-	//	return res.status(400).json({ error: "name must be unique" });
+	if (data.name === undefined)
+		return res.status(400).json({ error: "missing name" });
+	if (data.number === undefined)
+		return res.status(400).json({ error: "missing number" });
 
 	const person = new Person({ name: data.name, number: data.number });
 	person.save().then((savedPerson) => res.json(savedPerson));
 });
+
+app.put("/api/persons/:id", (req, res) => {
+	const data = req.body;
+
+	if (data.name === undefined)
+		return res.status(400).json({ error: "missing name" });
+	if (data.number === undefined)
+		return res.status(400).json({ error: "missing number" });
+
+	Person.findById(req.params.id)
+		.then((person) => {
+			if (!person) res.status(404).end();
+			person.name = data.name;
+			person.number = data.number;
+			return person.save().then((savedPerson) => res.json(savedPerson));
+		})
+		.catch((err) => next(err));
+});
+
+const unknownEndpoint = (req, res) => {
+	res.status(404).send({ error: "unknown endpoint" });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (err, req, res, next) => {
+	console.error(err.message);
+
+	if (err.name === "CastError") {
+		return res.status(400).send({ error: "malformatted id" });
+	}
+
+	next(err);
+};
+app.use(errorHandler);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
