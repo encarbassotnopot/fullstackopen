10145fd56f113d76ea1b74bc4ada3538bb7234c7
eina <git@eina.cc>
2026-06-12 14:35:57 +0200

    part5 base

---
+++ part5/backend/app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const log = require("./utils/logger");
+const config = require("./utils/config");
+const blogsRouter = require("./controllers/blogs");
+const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
+const middleware = require("./utils/middleware");
+
+const app = express();
+
+mongoose
+	.connect(config.MONGO_URI, { family: 4 })
+	.catch((e) => log.error("error connection to MongoDB:", e.message));
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
+
+app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ part5/backend/controllers/blogs.js
@@ -0,0 +1,55 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog");
+const middleware = require("../utils/middleware");
+
+blogsRouter.get("/", async (req, res) => {
+	const blogs = await Blog.find({}).populate("user", {
+		username: 1,
+		name: 1,
+	});
+
+	res.json(blogs);
+});
+
+blogsRouter.post("/", middleware.userExtractor, async (req, res) => {
+	const blog = new Blog(req.body);
+
+	blog.user = req.user.id;
+	const result = await blog.save();
+
+	req.user.blogs = req.user.blogs.concat(result._id);
+	await req.user.save();
+
+	res.status(201).json(result);
+});
+
+blogsRouter.put("/:id", middleware.userExtractor, async (req, res) => {
+	const blog = await Blog.findById(req.params.id);
+
+	if (!blog) return res.status(404).end();
+	if (blog.user.toString() !== req.user.id.toString())
+		return res
+			.status(401)
+			.json({ error: "note not owned by logged in user" });
+
+	blog.title = req.body.title;
+	blog.author = req.body.author;
+	blog.url = req.body.url;
+	blog.likes = req.body.likes;
+
+	res.json(await blog.save());
+});
+
+blogsRouter.delete("/:id", middleware.userExtractor, async (req, res) => {
+	const blog = await Blog.findById(req.params.id);
+	if (!blog) return res.status(204).end();
+	if (blog.user.toString() !== req.user.id.toString())
+		return res
+			.status(401)
+			.json({ error: "note not owned by logged in user" });
+
+	await Blog.deleteOne();
+	res.status(204).end();
+});
+
+module.exports = blogsRouter;
