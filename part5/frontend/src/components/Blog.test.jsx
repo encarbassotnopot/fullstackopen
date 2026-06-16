ab471de5ff6b8c81f51255421e99a91ba45895fe
eina <git@eina.cc>
2026-06-16 13:37:28 +0200

    tests fins 5.15

--- part5/frontend/src/components/Blog.test.jsx
+++ part5/frontend/src/components/Blog.test.jsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Blog from "./Blog";
 
-test("renders content", () => {
+describe("a blog entry", () => {
 	const user = {
 		name: "my name",
 		username: "myusername",
@@ -16,15 +17,46 @@ test("renders content", () => {
 		user: user,
 	};
 
-	render(<Blog blog={blog} user={user} />);
+	test("shows only title and author by default", () => {
+		render(<Blog blog={blog} user={user} />);
 
-	const title = screen.queryByText(blog.title);
-	const author = screen.queryByText(blog.author);
-	const url = screen.queryByText(blog.url);
-	const likes = screen.queryByText(blog.likes);
+		const title = screen.queryByText(blog.title);
+		const author = screen.queryByText(blog.author);
+		const url = screen.queryByText(blog.url);
+		const likes = screen.queryByText(blog.likes);
 
-	expect(title).toBeDefined();
-	expect(author).toBeDefined();
-	expect(url).toBeNull();
-	expect(likes).toBeNull();
+		expect(title).toBeDefined();
+		expect(author).toBeDefined();
+		expect(url).toBeNull();
+		expect(likes).toBeNull();
+	});
+
+	test("can be expanded to show url and number of likes", async () => {
+		render(<Blog blog={blog} user={user} />);
+
+		const testAgent = userEvent.setup();
+		const button = screen.getByText("view");
+		await testAgent.click(button);
+
+		const url = screen.queryByText(blog.url);
+		const likes = screen.queryByText(`Likes ${blog.likes}`);
+
+		expect(url).toBeDefined();
+		expect(likes).toBeDefined();
+	});
+
+	test("has a like button that can be clicked twice", async () => {
+		const mockHandler = vi.fn();
+
+		render(<Blog blog={blog} user={user} handleLike={mockHandler} />);
+
+		const testAgent = userEvent.setup();
+		const viewButton = screen.getByText("view");
+		await testAgent.click(viewButton);
+		const likeButton = screen.getByText("like");
+		await testAgent.click(likeButton);
+		await testAgent.click(likeButton);
+
+		expect(mockHandler.mock.calls).toHaveLength(2);
+	});
 });
