499aaf258f6ea1afe13b411ccea23eef71f69d15
eina <git@eina.cc>
2026-06-18 14:25:50 +0200

    unicafe zustand

---
+++ part6/unicafe-zustand/src/components/Buttons.jsx
@@ -0,0 +1,16 @@
+import { useFeedbackControls } from "./store";
+
+const Buttons = () => {
+	const { addGood, addBad, addNeutral } = useFeedbackControls();
+
+	return (
+		<div>
+			<h2>give feedback</h2>
+			<button onClick={addGood}>good</button>
+			<button onClick={addNeutral}>neutral</button>
+			<button onClick={addBad}>bad</button>
+		</div>
+	);
+};
+
+export default Buttons;
---
+++ part6/unicafe-zustand/src/components/store.js
@@ -0,0 +1,37 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+const useFeedbackStore = create(
+	devtools((set) => ({
+		feedback: { good: 0, neutral: 0, bad: 0 },
+		actions: {
+			addGood: () =>
+				set((state) => ({
+					feedback: {
+						...state.feedback,
+						good: state.feedback.good + 1,
+					},
+				})),
+			addNeutral: () =>
+				set((state) => ({
+					feedback: {
+						...state.feedback,
+						neutral: state.feedback.neutral + 1,
+					},
+				})),
+			addBad: () =>
+				set((state) => ({
+					feedback: {
+						...state.feedback,
+						bad: state.feedback.bad + 1,
+					},
+				})),
+		},
+	}))
+);
+
+// the hook functions that are used elsewhere in app
+export const useFeedback = () => useFeedbackStore((state) => state.feedback);
+
+export const useFeedbackControls = () =>
+	useFeedbackStore((state) => state.actions);
