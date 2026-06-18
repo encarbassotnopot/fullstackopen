b2e96b0708be2867c4e872af99520b3c22406ceb
eina <git@eina.cc>
2026-06-18 10:58:15 +0200

    part 5 feta, single blog card

--- part5/frontend/src/components/Blog.jsx
+++ part5/frontend/src/components/Blog.jsx
@@ -1,26 +1,51 @@
-const Blog = ({ user, blog, handleLike, handleDelete }) => {
-	const deleteButton = () => {
-		if (user?.id === blog.user.id)
-			return <button onClick={() => handleDelete(blog)}>remove</button>;
-	};
+import {
+	Button,
+	Card,
+	CardContent,
+	CardActions,
+	Typography,
+	Link,
+} from "@mui/material";
 
+const Blog = ({ user, blog, handleLike, handleDelete }) => {
 	if (!blog) return null;
 
 	return (
-		<div>
-			<h2>
-				{blog.author}: {blog.title}
-			</h2>
-			<div>
-				<a href={blog.url}>{blog.url}</a>
-			</div>
-			<div>
-				Likes {blog.likes}{" "}
-				{user && <button onClick={() => handleLike(blog)}>like</button>}
-			</div>
-			<div>Added By {blog.user.name}</div>
-			<div>{deleteButton()}</div>
-		</div>
+		<Card style={{ marginTop: 20, padding: 10 }}>
+			<CardContent>
+				<Typography variant="h2" sx={{ fontSize: 30 }}>
+					{blog.title}
+				</Typography>
+				<Typography sx={{ color: "text.secondary" }}>
+					{blog.author}
+				</Typography>
+				<Link href={blog.url}>{blog.url}</Link>
+				<Typography sx={{ color: "text.secondary" }}>
+					Added by {blog.user.name}
+				</Typography>
+			</CardContent>
+			<CardActions>
+				<Typography>Likes {blog.likes}</Typography>
+				{user && (
+					<Button
+						color="primary"
+						variant="outlined"
+						onClick={() => handleLike(blog)}
+					>
+						like
+					</Button>
+				)}
+				{user?.id === blog.user.id && (
+					<Button
+						color="error"
+						variant="outlined"
+						onClick={() => handleDelete(blog)}
+					>
+						remove
+					</Button>
+				)}
+			</CardActions>
+		</Card>
 	);
 };
 export default Blog;
